b97c90cdf36dcfe1d54ff25c8987008f0ea46fc7
Andre2828Ferreira88 <123772724+Andre2828Ferreira88@users.noreply.github.com>
2023-08-18 11:02:04 -0300

    Add files via upload

---
+++ treino.js
@@ -0,0 +1,8 @@
+const caixinha = ["Treinar", "Volei" , "Apostas"];
+let posicao = caixinha.indexOf("Treinar")
+caixinha.splice(2)
+caixinha.pop(1);
+caixinha.push("Cozinhar")
+caixinha[0] = "correr"
+console.log(caixinha)
+
